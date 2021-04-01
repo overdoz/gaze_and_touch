d8bb2f76f1bdb0c03e231c2efbbf9094bc5fd4f9
overdoz <thanh-le@hotmail.de>
2021-04-01 11:01:49 +0200

    add widget size

--- lib/utils/widgetDetails.dart
+++ lib/utils/widgetDetails.dart
@@ -1,9 +1,11 @@
-static import 'package:flutter/material.dart';
-
 import 'package:flutter/material.dart';
-
 
 Offset getWidgetPosition(GlobalKey key) {
   final RenderBox renderBox = key.currentContext.findRenderObject();
   return renderBox.localToGlobal(Offset.zero);
+}
+
+Size getWidgetSize(GlobalKey key) {
+  final RenderBox renderBox = key.currentContext.findRenderObject();
+  return renderBox.size;
 }
