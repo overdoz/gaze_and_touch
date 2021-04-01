920430651ee1259863bbafdee36155eb3aa2e8a5
overdoz <thanh-le@hotmail.de>
2021-04-01 13:46:21 +0200

    determine widget position before render

--- lib/screens/buttons.dart
+++ lib/screens/buttons.dart
@@ -1,63 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:gazeAndTouch/utils/widgetDetails.dart';
 
-class ButtonsScreen extends StatelessWidget {
-  GlobalKey key1 = GlobalKey();
-  GlobalKey key2 = GlobalKey();
-  GlobalKey key3 = GlobalKey();
+
+class ButtonsScreen extends StatefulWidget {
+  ButtonsScreen({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _ButtonsScreenState createState() => _ButtonsScreenState();
+}
+
+class _ButtonsScreenState extends State<ButtonsScreen> {
+  final GlobalKey key1 = GlobalKey();
+  final GlobalKey key2 = GlobalKey();
+  final GlobalKey key3 = GlobalKey();
+  final GlobalKey backButtonKey = GlobalKey();
+
+  var backButtonPosition;
+  var backButtonSize;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      backButtonPosition = getWidgetPosition(backButtonKey);
+      backButtonSize = getWidgetSize(backButtonKey);
+      print(getWidgetPosition(backButtonKey));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Buttons'),
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Buttons'),
+          leading: IconButton(
+            key: backButtonKey,
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {
+              print(getWidgetPosition(backButtonKey));
+              print(getWidgetSize(backButtonKey));
+              Navigator.of(context).pop();
+            },
+          ),
+        ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: Text('Open route'),
+              key: key1,
+              onPressed: () {
+                var position = getWidgetPosition(key1);
+                var size = getWidgetSize(key1);
+                print(position);
+                print(size);
+              },
+            ),
+            ElevatedButton(
+              child: Text('Open route'),
+              key: key2,
+              onPressed: () {},
+            ),
+            ElevatedButton(
+              child: Text('Open route'),
+              key: key3,
+              onPressed: () {},
+            ),
+          ],
+        )),
       ),
-      body: Center(
-          child: Column(
-        children: [
-          ElevatedButton(
-            child: Text('Open route'),
-            key: key1,
-            onPressed: () {
-              // Navigator.pop(context);
-              final RenderBox renderBoxRed =
-                  key1.currentContext.findRenderObject();
-              final sizeRed = renderBoxRed.size;
-              Offset position = renderBoxRed
-                  .localToGlobal(Offset.zero); //this is global position
-              // double y = position.dy;
-              print(position);
-            },
-          ),
-          ElevatedButton(
-            child: Text('Open route'),
-            key: key2,
-            onPressed: () {
-              // Navigator.pop(context);
-              final RenderBox renderBoxRed =
-                  key2.currentContext.findRenderObject();
-              final sizeRed = renderBoxRed.size;
-              Offset position = renderBoxRed
-                  .localToGlobal(Offset.zero); //this is global position
-              // double y = position.dy;
-              print(position);
-            },
-          ),
-          ElevatedButton(
-            child: Text('Open route'),
-            key: key3,
-            onPressed: () {
-              // Navigator.pop(context);
-              final RenderBox renderBoxRed =
-                  key3.currentContext.findRenderObject();
-              final sizeRed = renderBoxRed.size;
-              Offset position = renderBoxRed
-                  .localToGlobal(Offset.zero); //this is global position
-              // double y = position.dy;
-              print(position);
-            },
-          ),
-        ],
-      )),
     );
   }
 }
