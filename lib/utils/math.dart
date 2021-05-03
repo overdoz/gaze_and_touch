2af909cb1facfb25ecd4281ba4c0f00183d97f96
overdoz <thanh-le@hotmail.de>
2021-05-03 18:26:04 +0200

    calculate screen measures

--- lib/utils/math.dart
+++ lib/utils/math.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 double mapNum(double input, double inputA, double inputZ, double outputA, double outputZ) {
   return (input - inputA) / (inputZ - inputA) * (outputZ - outputA) + outputA;
 }
@@ -5,6 +7,27 @@ double mapNum(double input, double inputA, double inputZ, double outputA, double
 Map<String, double> calcMeasurements(screenSize, aspectRatio, mobileScreenSize, mobileAspectRatio) {
   var measurements = new Map();
 
+  var heightScreen = screenSize / sqrt(pow(aspectRatio, 2) + 1);
+  var widthScreen = aspectRatio * heightScreen;
+
+  var heightMobile = mobileScreenSize / sqrt(pow(mobileAspectRatio, 2) + 1);
+  var widthMobile = aspectRatio * heightMobile;
+
+  var marginHorizontal = (widthScreen - widthMobile) / 2;
+  var marginVertical = (heightScreen - heightMobile) / 2;
+
+  var inputAWidth = marginHorizontal / widthScreen * 100;
+  var inputBWidth = inputAWidth + (widthMobile / widthScreen * 100);
+
+  var inputAHeight = marginVertical / heightScreen * 100;
+  var inputBHeight = inputAHeight + (heightMobile / heightScreen * 100);
+
+  /// TODO: get aspect ratio from context
+
+  measurements["inputA width"] = inputAWidth;
+  measurements["inputB width"] = inputBWidth;
+  measurements["inputA height"] = inputAHeight;
+  measurements["inputB height"] = inputBHeight;
 
 
   return measurements;
